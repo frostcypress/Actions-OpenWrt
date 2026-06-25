7ef5fad3ba90a026b0f8c5b5a48e3fb01149a719
Chen <288239462+frostcypress@users.noreply.github.com>
2026-06-25 09:05:35 +0800

    Update Part1_Feeds.sh

--- Part1_Feeds.sh
+++ Part1_Feeds.sh
@@ -18,7 +18,8 @@
 # LEDE (master)
 cat >> feeds.conf.default << EOF
 src-git packages https://github.com/coolsnowwolf/packages
-src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12
+src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05
+#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12
 src-git routing https://github.com/coolsnowwolf/routing
 src-git telephony https://github.com/coolsnowwolf/telephony.git
 EOF
