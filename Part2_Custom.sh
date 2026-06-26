042fbb08fe0ab6b28f061a30f4839b4cbdbc57c9
Chen <288239462+frostcypress@users.noreply.github.com>
2026-06-26 11:16:54 +0800

    Update Part2_Custom.sh

--- Part2_Custom.sh
+++ Part2_Custom.sh
@@ -41,6 +41,6 @@ CRYPT_PASSWORD=$(openssl passwd -1 "$NEW_PASSWORD")
 sed -i "s|^root:[^:]*:|root:$CRYPT_PASSWORD:|" $CFG_PATH_2
 
 echo "⚠️ 配置完成，正在验证..."
-grep -E 'CCACHE|vlmcsd|ddns|wol|etherwake|apk|ipk' .config
+grep -E 'CCACHE|vlmcsd|ddns|wol|etherwake|apk|ipk|libwebsockets-mbedtls|libwebsockets-openssl' .config
 grep -E 'root' $CFG_PATH_2
 grep -E "ipaddr" $CFG_PATH_1
