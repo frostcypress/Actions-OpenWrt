9ae757742a8a6218df574ebf1134978751a21d7a
Chen <288239462+frostcypress@users.noreply.github.com>
2026-06-29 09:57:38 +0800

    Update Part1_Feeds.sh

--- Part1_Feeds.sh
+++ Part1_Feeds.sh
@@ -11,26 +11,27 @@
 > feeds.conf.default
 
 # LEDE (master)
-#cat >> feeds.conf.default << EOF
-#src-git packages https://github.com/coolsnowwolf/packages
-#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05
+cat >> feeds.conf.default << EOF
+src-git packages https://github.com/coolsnowwolf/packages
+src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05
 #src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10
 #src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12
-#src-git routing https://github.com/coolsnowwolf/routing
+src-git routing https://github.com/coolsnowwolf/routing
+src-git telephony https://git.openwrt.org/feed/telephony.git
 #src-git telephony https://github.com/coolsnowwolf/telephony.git
 #src-git helloworld https://github.com/fw876/helloworld.git
 #src-git rtp2httpd https://github.com/stackia/rtp2httpd.git
-#EOF
+EOF
 
 # LibWrt（main-nss）
-cat >> feeds.conf.default << EOF
-src-git nss_packages https://github.com/qosmio/nss-packages.git
-src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git
-src-git packages https://github.com/immortalwrt/packages.git
-src-git luci https://github.com/frostcypress/luci.git
-src-git routing https://github.com/openwrt/routing.git
-src-git telephony https://github.com/openwrt/telephony.git
-src-git video https://github.com/openwrt/video.git
+#cat >> feeds.conf.default << EOF
+#src-git nss_packages https://github.com/qosmio/nss-packages.git
+#src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git
+#src-git packages https://github.com/immortalwrt/packages.git
+#src-git luci https://github.com/frostcypress/luci.git
+#src-git routing https://github.com/openwrt/routing.git
+#src-git telephony https://github.com/openwrt/telephony.git
+#src-git video https://github.com/openwrt/video.git
 #src-git helloworld https://github.com/fw876/helloworld.git
-EOF
+#EOF
 
