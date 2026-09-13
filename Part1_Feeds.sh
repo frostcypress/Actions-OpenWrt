5ec54ca03c3c06252f22575a94574e6baf5b555e
Chen <288239462+frostcypress@users.noreply.github.com>
2026-09-13 10:21:15 +0800

    Update feed sources in Part1_Feeds.sh

--- Part1_Feeds.sh
+++ Part1_Feeds.sh
@@ -12,32 +12,9 @@
 
 # LEDE (master)
 cat >> feeds.conf.default << EOF
-src-git packages https://github.com/coolsnowwolf/packages
-src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10
-#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12
-src-git routing https://github.com/coolsnowwolf/routing
+src-git packages https://github.com/coolsnowwolf/packages.git
+src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12
+src-git routing https://github.com/coolsnowwolf/routing.git
 src-git telephony https://github.com/coolsnowwolf/telephony.git
-#src-git helloworld https://github.com/fw876/helloworld.git
+src-git helloworld https://github.com/fw876/helloworld.git
 EOF
-
-# LEDE (20251001)
-#cat >> feeds.conf.default << EOF
-#src-git rtp2httpd https://github.com/stackia/rtp2httpd.git;v3.15.3
-#src-git packages https://github.com/coolsnowwolf/packages
-#src-git luci https://github.com/frostcypress/luci.git;openwrt-23.05
-#src-git routing https://github.com/coolsnowwolf/routing
-#src-git telephony https://github.com/coolsnowwolf/telephony.git
-#src-git helloworld https://github.com/fw876/helloworld.git
-#EOF
-
-
-# LibWrt（25.12-nss）
-#cat >> feeds.conf.default << EOF
-#src-git nss_packages https://github.com/qosmio/nss-packages.git
-#src-git sqm_scripts_nss https://github.com/qosmio/sqm-scripts-nss.git
-#src-git packages https://github.com/immortalwrt/packages.git;openwrt-25.12
-#src-git luci https://github.com/immortalwrt/luci.git;openwrt-25.12
-#src-git routing https://github.com/openwrt/routing.git;openwrt-25.12
-#src-git telephony https://github.com/openwrt/telephony.git;openwrt-25.12
-#src-git video https://github.com/openwrt/video.git;openwrt-25.12
-#EOF
